e1d69db3db55febbb704a06752019dc374aecc55
Koinunopochi <koinunopoti@gmail.com>
2024-06-04 18:33:27 +0900

    fix：ディレクトリの中を整理した

--- sample_next/src/app/home/page.tsx
+++ sample_next/src/app/home/page.tsx
@@ -5,6 +5,8 @@ export default function HomePage(){
       <h4>
         Welcome to the home page
       </h4>
+      <p>本来は、ユーザ―用の画面に切り替える。</p>
+      <p>ログインユーザーでないとアクセスできないようにする。</p>
     </div>
   );
 }
--- sample_next/src/app/page.tsx
+++ sample_next/src/app/page.tsx
@@ -1,6 +1,8 @@
 export default function Home() {
   return (
     <main>
+      <h1>Top Page</h1>
+      <p>This is Top Page !!</p>
       <a href="./auth/signin">signin</a>
     </main>
   )
